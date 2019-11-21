cb3ae749e116b6224b2d91420601fa5063e65834
N4de <jaAnnala@gmail.com>
2019-11-21 20:04:17 +0200

    6.8: anekdootit, step6

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 
-import {voteForAnecdote} from './reducers/anecdoteReducer';
 import AnecdoteForm from './components/AnecdoteForm';
+import AnecdoteList from './components/AnecdoteList';
 
 const App = (props) => {
-  const anecdotes = props.store.getState();
-  const store = props.store;
-
-  const vote = (id) => {
-    console.log('vote', id)
-    store.dispatch(voteForAnecdote(id));
-  }
 
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList 
+        store={props.store}
+      />
       <AnecdoteForm 
         store={props.store}
       />
